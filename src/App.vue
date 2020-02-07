633711f01f0027d33470d992939d2cc1905cdf2e
egunyoa <yang.a.guo@ericsson.com>
2020-02-07 15:47:32 +0100

    fix layout

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,13 @@
           <a href="/">{{ $t('home') }}</a>
         </li>
         <li class="nav-item">
-          <a href="">{{ $t('download') }}</a>
+          <a href="#download">{{ $t('download') }}</a>
         </li>
         <li class="nav-item">
-          <a href="https://github.com/FlowCI/docs">{{ $t('document') }}</a>
+          <a href="https://github.com/FlowCI/docs" target="_blank">{{ $t('document') }}</a>
         </li>
         <li class="nav-item">
-          <a href="https://github.com/FlowCI/docs/issues">{{ $t('issue') }}</a>
+          <a href="https://github.com/FlowCI/docs/issues" target="_blank">{{ $t('issue') }}</a>
         </li>
         <li>
           <a class="btn btn-github" href="https://github.com/flowci">
@@ -38,8 +38,10 @@
               <li>{{ $t('slogan.plugin') }}</li>
             </ul>
             <div>
-              <button class='btn btn-guide'>{{ $t('start') }}</button>
-              <button class='btn btn-download'>{{ $t('download') }}</button>
+              <a class='btn btn-guide'
+                 href="https://github.com/FlowCI/docs/blob/master/v1.0/start/index.md"
+                 target="_blank">{{ $t('start') }}</a>
+              <a class='btn btn-download' href="#download">{{ $t('download') }}</a>
             </div>
           </div>
         </div>
@@ -53,17 +55,17 @@
         {{ $t('desc.3') }}<br />
       </p>
       <div class="gradation-box">
-        <div class="download-group">
+        <div id="download" class="download-group">
           <h2>{{ $t('download') }}</h2>
           <div class="download-version">V1.0 Beta</div>
           <div class="download-actions">
-            <a>
+            <a href="https://github.com/flowci/docker" target="_blank">
               <i class="icon icon-download"></i>
-              安装包
+              Docker
             </a>
             <a href="https://github.com/flowci">
               <i class="icon icon-github"></i>
-              源码
+              Source Code
             </a>
           </div>
         </div>
@@ -110,9 +112,20 @@
 </template>
 
 <script>
+import axios from 'axios'
 
 export default {
-  name: 'app'
+  name: 'app',
+  mounted () {
+    axios.get('https://raw.githubusercontent.com/FlowCI/docs/master/CHANGELOG.md')
+      .then((r) => {
+        if (r.status !== 200) {
+          return
+        }
+
+        console.log(r.data)
+      })
+  }
 }
 </script>
 
